9334e2773962afe9dfb7d5d9ecfd379b63cf172e
Alexandr Bychkov <dfcz179@yandex.ru>
2022-12-01 00:55:45 +0500

    Replace multiplication on division

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main()
     cout << "Enter two integer number" << endl;
     cin >> a >> b;
     cout << "a+b=" << a+b << endl;
-
-cout << "a-b=" << a-b << endl;
+    cout << "a-b=" << a-b << endl;
+    cout << "a/b=" << a/b <<endl;
 
 }
